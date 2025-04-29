6f2b07f159bb005645e4969a6036873842302a8a
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-04-29 09:58:14 +0700

    question-3.js done

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: มี Big O เป้น O(n) เพราะมีการวนลูปหาอย่างน้อยเกินครึ่งรอบ
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: มี Big O เป้น O(n) เพราะมีการวนลูปหาอย่างน้อยเกินครึ่งรอบ
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: เป็น Big O notation ชนิดเดียวกัน ไม่เหมาะกับ array ใหญ่ไ และข้อมูลใหญ่
 
 
 */
