ab52f6e73dffef04b73b8aa3d0b320fa5a96e768
Sanya Bochoun <76147037+sanya-bochoun@users.noreply.github.com>
2025-04-29 10:54:23 +0700

    question-5.js done

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer: มี Big O คือ O(n^2) เพราะมีข้อมูลที่เรียงย้อนกัน และสลับหาข้อมูลทุกคู่
+Answer: มี Big O คือ O(n^2) เพราะมีข้อมูลที่เรียงย้อนกัน และสลับหาข้อมูลทุกคู่ 
 
 
 */
